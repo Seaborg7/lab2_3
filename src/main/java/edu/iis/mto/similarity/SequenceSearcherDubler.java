0824068adc93af1243738b6f24a7263daba66622
Seaborg7 <seaborg7@tlen.pl>
2019-04-03 01:26:15 +0200

    Dodany counter, testy countera, dodane testy na liczbach ujemnych

--- src/main/java/edu/iis/mto/similarity/SequenceSearcherDubler.java
+++ src/main/java/edu/iis/mto/similarity/SequenceSearcherDubler.java
@@ -4,12 +4,15 @@ import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
 public class SequenceSearcherDubler implements SequenceSearcher {
+    
+    private int counter=0;
 
     @Override
     public SearchResult search(int key, int[] seq) {
         SearchResult.Builder builder = SearchResult.builder();
         for (int i = 0; i < seq.length; i++) {
             if (seq[i] == key) {
+                counter++;
                 return builder.withFound(true)
                               .withPosition(i)
                               .build();
@@ -17,6 +20,10 @@ public class SequenceSearcherDubler implements SequenceSearcher {
         }
         return builder.withFound(false)
                       .build();
+    }
+    
+    public int getCounter() {
+        return counter;
     }
 
 }
--- src/test/java/lab2_3/Test.java
+++ src/test/java/lab2_3/Test.java
@@ -12,6 +12,8 @@ public class Test {
     private int one[] = new int[] {1};
     private int onetwo[] = new int[] {1, 2};
     private int nullseq[] = new int[] {};
+    private int neg1[] = new int[] {-5, -7};
+    private int neg2[] = new int[] {-7, -10};
     private float delta = (float) 0.01;
     SequenceSearcherDubler searcher = new SequenceSearcherDubler();
     SimilarityFinder similarityFinder = new SimilarityFinder(this.searcher);
@@ -55,6 +57,29 @@ public class Test {
     public void oneTwoOneSeq() {
         double result = similarityFinder.calculateJackardSimilarity(onetwo, one);
         Assert.assertEquals(0.5, result, delta);
+
+    }
+    
+    @org.junit.Test
+    public void twoSameLengthCounterTest() {
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        int counter = searcher.getCounter();
+        Assert.assertEquals(counter, seq1.length);
+
+    }
+    
+    @org.junit.Test
+    public void bothNullCounterTest() {
+        double result = similarityFinder.calculateJackardSimilarity(nullseq, nullseq);
+        int counter = searcher.getCounter();
+        Assert.assertEquals(counter, nullseq.length);
+
+    }
+    
+    @org.junit.Test
+    public void diffNegativeSequence() {
+        double result = similarityFinder.calculateJackardSimilarity(neg1, neg2);
+        Assert.assertEquals(1.0/3, result, delta);
 
     }
 
