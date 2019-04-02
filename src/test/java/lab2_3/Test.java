c04d4635c19abba306c7022dc9e3ebd33307669a
Seaborg7 <seaborg7@tlen.pl>
2019-04-03 01:05:23 +0200

    Rozszerzenie testów

--- src/test/java/lab2_3/Test.java
+++ src/test/java/lab2_3/Test.java
@@ -8,7 +8,10 @@ public class Test {
 
     private int seq1[] = new int[] {1, 2, 3};
     private int seq2[] = new int[] {1, 2, 3};
-    private int nullseq[] = new int [] {};
+    private int seq3[] = new int[] {6, 7, 8};
+    private int one[] = new int[] {1};
+    private int onetwo[] = new int[] {1, 2};
+    private int nullseq[] = new int[] {};
     private float delta = (float) 0.01;
     SequenceSearcherDubler searcher = new SequenceSearcherDubler();
     SimilarityFinder similarityFinder = new SimilarityFinder(this.searcher);
@@ -24,6 +27,34 @@ public class Test {
     public void nullSequence() {
         double result = similarityFinder.calculateJackardSimilarity(seq1, nullseq);
         Assert.assertEquals(0.0, result, delta);
+
+    }
+
+    @org.junit.Test
+    public void diffSequence() {
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq3);
+        Assert.assertEquals(0.0, result, delta);
+
+    }
+    
+    @org.junit.Test
+    public void bothNullSequence() {
+        double result = similarityFinder.calculateJackardSimilarity(nullseq, nullseq);
+        Assert.assertEquals(1.0, result, delta);
+
+    }
+    
+    @org.junit.Test
+    public void oneOneTwoSeq() {
+        double result = similarityFinder.calculateJackardSimilarity(one, onetwo);
+        Assert.assertEquals(0.5, result, delta);
+
+    }
+    
+    @org.junit.Test
+    public void oneTwoOneSeq() {
+        double result = similarityFinder.calculateJackardSimilarity(onetwo, one);
+        Assert.assertEquals(0.5, result, delta);
 
     }
 
